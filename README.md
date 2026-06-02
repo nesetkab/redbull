0c1e33f6e87d4240e19e4b0039f9921d25fe1cca
nesetkab <foulofdoom@gmail.com>
2026-06-01 23:41:10 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Red Bull Tracker
+uses Svelte
+
+i made this so i can track how unhealthy i am :) 
+
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<div class="bg-bg min-h-screen flex flex-col p-8">
+  <div class="flex flex-row min-w-full">
+    <div class="flex-grow">
+      <h1 class="text-4xl text-text font-bold">Red Bull Tracker</h1>
+      <h2 class="text-xl text-text">to track how unhealthy i am :3</h2>
+    </div>
+    <h2 class="text-xl text-text">Today's Red Bulls: {0}</h2>
+  </div>
+</div>
